27a4b58013de0913258a8001c1534db4fce7fb95
Amyssia <manon.lamagnere@gmail.com>
2018-03-21 10:56:18 +0100

    1.0

---
+++ Assets/Scenes/UI_Manager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour
+{
+    public float LastTimeScale;
+    public GameObject PausePanel;
+    public string PlayScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void OnClickResume()
+    {
+        TogglePause();
+    }
+
+    public void OnClickReload()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(PlayScene);
+        
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void TogglePause()
+    {
+        if(PausePanel.activeSelf)
+        {
+            PausePanel.SetActive(false);
+            Time.timeScale = LastTimeScale;
+        }
+        else if (!PausePanel.activeSelf)
+        {
+            LastTimeScale = Time.timeScale;
+            PausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+
+
+
+}
